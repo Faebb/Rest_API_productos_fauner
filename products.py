4d35bf5a6329b562245ddee2275efdbd142205ee
Faebb <faeb265@gmail.com>
2023-08-29 11:15:41 -0500

    eliminacion de proyecto anterior

--- products.py
+++
@@ -1,7 +0,0 @@
-#archivo para simular los datos, que normalmente vienen de una base de datos
-#listas de productos en objetos
-products = [
-    {"name": "laptop", "price": 800, "quantity": 4},
-    {"name": "mouse", "price": 40, "quantity": 10},
-    {"name": "monitor", "price": 400, "quantity": 3}
-]
